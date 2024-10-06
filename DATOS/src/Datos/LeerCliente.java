d730692133c541fa10a3be7b2ef037679cb1fcce
Owelillo <owelcontacto@gmail.com>
2024-10-06 14:43:01 +0200

    Cosas pc

--- DATOS/src/Datos/LeerCliente.java
+++ DATOS/src/Datos/LeerCliente.java
@@ -6,34 +6,24 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.io.Serializable;
 
-public class LeerCliente implements Serializable{
+public class LeerCliente implements Serializable {
 
-	public static void main(String[] args) throws FileNotFoundException{
+	public static void main(String[] args) throws FileNotFoundException {
 		File fichero = new File("altacoche.java");
 		RandomAccessFile file = new RandomAccessFile(fichero, "rw");
 		RandomAccessFile fileread = new RandomAccessFile(fichero, "r");
-		String dnis[] = {
-				"71457145J", "52547854L", "45254578J"
-		};
-		String nombres[] = {
-				"Juan", "Adela", "Cristina"
-		};
-		String apellidos[] = {
-				"Salas Acebes", "Simone Saludes", "Alcoy Zaldivar"
-		};
-		String matriculas[] = {
-				"5254HFR", "8578FGH", "5652DFR"
-		};
-		String ciudades[] = {
-				"Sevilla", "Zaragoza", "Zamora"
-		};
+		String dnis[] = { "71457145J", "52547854L", "45254578J" };
+		String nombres[] = { "Juan", "Adela", "Cristina" };
+		String apellidos[] = { "Salas Acebes", "Simone Saludes", "Alcoy Zaldivar" };
+		String matriculas[] = { "5254HFR", "8578FGH", "5652DFR" };
+		String ciudades[] = { "Sevilla", "Zaragoza", "Zamora" };
 		int posicion = 90;
 		char apellido[] = new char[13], aux;
-		
-		for(;;) {
+
+		for (;;) {
 			try {
 				file.seek(posicion);
-				for(int i = 0; i<apellido.length; i++) {
+				for (int i = 0; i < apellido.length; i++) {
 					aux = file.readChar();
 					apellido[i] = aux;
 				}
